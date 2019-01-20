50c54e192d035f9ad600b4dc551caa1ba2170411
ayalura0312 <aayalur3@illinois.edu>
2019-01-20 12:45:28 -0600

    Completed Invalid Input Methods

--- src/com/example/TicTacToe.java
+++ src/com/example/TicTacToe.java
@@ -4,6 +4,35 @@ public class TicTacToe {
 
     public static Evaluation evaluateBoard(String boardState) {
         // implement your code here
-        //
+
+        //Invalid Input
+        if(boardState == null || boardState.length() == 0 || boardState.length() != 9) {
+            return Evaluation.InvalidInput;
+        }
+
+        //UnreachableState
+        char[] charArray = boardState.toCharArray();
+        int xCounter = 0;
+        int oCounter= 0;
+        for (int i = 0; i < charArray.length; i++) {
+            if (charArray[i] == 'x' || charArray[i] == 'X') {
+                xCounter++;
+            }
+            if (charArray[i] == 'o' || charArray[i] == 'O') {
+                oCounter++;
+            }
+        }
+        if (xCounter == 9 || oCounter == 9) {
+            return Evaluation.UnreachableState;
+        }
+        return Evaluation.InvalidInput;
+
+
+
+        //X-Wins
+
+        //O-Wins
+
+        //No Winner
     }
 }
